732b308c3f463fbbc8b7834dd735b0069119607b
BillaSpace <SmokenX1@gmail.com>
2025-07-13 16:14:58 +0530

    Update seek.py

--- Opus/plugins/admins/seek.py
+++ Opus/plugins/admins/seek.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("seek.log"),
+        logging.FileHandler("seekpy.log"),
         logging.StreamHandler()
     ]
 )
